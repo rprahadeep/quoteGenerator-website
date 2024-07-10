cafe656fc1e34706ed4b3cc18df63a6a93d1fdf9
Prahadeep R <145466342+rprahadeep@users.noreply.github.com>
2024-07-10 14:56:54 +0530

    quotes Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,28 @@ const quotes = [{
 {
     quote: '“The time is always right to do what is right.”',
     person: 'Martin Luther King. Jr'
-},];
+},
+{
+    quote: '"All that glitters is not gold."',
+    person: 'William Shakespeare'
+},
+{
+    quote: '"Genius is one percent inspiration and ninety-nine percent perspiration."',
+    person: 'Thomas Edison'
+},
+{
+    quote: '"If you are going through hell, keep going."',
+    person: 'Winston Churchill'
+},
+{
+    quote: '"If you want something said, ask a man; if you want something done, ask a woman."',
+    person: 'Margaret Thatcher'
+},
+{
+    quote: '"That’s one small step for a man, a giant leap for mankind."',
+    person: 'Neil Armstrong'
+},
+];
 btn.addEventListener('click', function(){
     const quote1 = document.querySelector('.quote');
     const person1 = document.querySelector('.person');
